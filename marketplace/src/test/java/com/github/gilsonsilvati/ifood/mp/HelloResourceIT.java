e176a998be5f4e0e61e6a37e8bd87a438de0a2e2
Gilson Silva <gilsonsilvati@gmail.com>
2022-08-27 23:31:23 -0300

    first commit

---
+++ marketplace/src/test/java/com/github/gilsonsilvati/ifood/mp/HelloResourceIT.java
@@ -0,0 +1,8 @@
+package com.github.gilsonsilvati.ifood.mp;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloResourceIT extends HelloResourceTest {
+    // Execute the same tests but in packaged mode.
+}
